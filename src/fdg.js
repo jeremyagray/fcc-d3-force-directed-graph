64a8b4fc72bac5e36413b4dcf3fc226f219bcaa4
Jeremy A Gray <jeremy.a.gray@gmail.com>
2022-04-20 11:28:44 -0500

    feat(d3): add initial simulation
    Add initial simulation based on Mike Bostock's
    example (https://observablehq.com/@d3/force-directed-graph) with
    additional descriptive attributes and tooltips.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- src/fdg.js
+++ src/fdg.js
@@ -11,7 +11,6 @@
  */
 
 d3.json('data.json').then((deps) => {
-
   // Dimensions.
   const dimensions = {
     height: 800,
@@ -26,12 +25,30 @@ d3.json('data.json').then((deps) => {
   dimensions.vCenter = dimensions.width / 2;
 
   // Tooltip.
-  const div = d3.select('body')
+  const tooltip = d3.select('body')
         .append('div')
         .attr('id', 'tooltip')
         .style('opacity', '0')
         .style('display', 'none')
         .style('visibility', 'hidden');
+
+  // Construct the forces.
+  const forceNode = d3.forceManyBody();
+  const forceLink = d3.forceLink(deps.links).id((d) => {
+    return d['dependency'];
+  });
+
+  const nodeStrength = -5;
+  const linkStrength = 0.1;
+
+  forceNode.strength(nodeStrength);
+  forceLink.strength(linkStrength);
+
+  const simulation = d3.forceSimulation(deps.nodes)
+        .force("link", forceLink)
+        .force("charge", forceNode)
+        .force("center",  d3.forceCenter())
+        .on("tick", ticked);
 
   // SVG canvas.
   const svg = d3.select("div#visualization")
@@ -39,5 +56,105 @@ d3.json('data.json').then((deps) => {
         .attr("id", "canvas")
         .attr("width", dimensions.width)
         .attr("height", dimensions.height)
-        .style('background-color', '#005900');
+        .attr("viewBox", [-dimensions.width / 2, -dimensions.height / 2, dimensions.width, dimensions.height])
+        .style('background-color', '#ffffff');
+
+  const link = svg.append("g")
+        .attr("stroke", '#ff0000')
+        .attr("stroke-opacity", 100)
+        .attr("stroke-width", 1)
+        .attr("stroke-linecap", 'round')
+        .selectAll("line")
+        .data(deps.links)
+        .join("line")
+        .attr('class', 'link')
+        .attr('data-source', (d) => {
+          return d['source'];
+        })
+        .attr('data-target', (d) => {
+          return d['target'];
+        })
+        .attr('data-links', (d) => {
+          return d['links'];
+        });
+
+  const node = svg.append("g")
+        .attr("fill", '#000000')
+        .attr("stroke", '#000000')
+        .attr("stroke-opacity", 100)
+        .attr("stroke-width", 1)
+        .selectAll("circle")
+        .data(deps.nodes)
+        .join("circle")
+        .attr('class', 'node')
+        .attr("r", 5)
+        .attr('data-dependency', (d) => {
+          return d['dependency'];
+        })
+        .on('mouseenter mouseover', (event, datum) =>
+          {
+            tooltip
+	      .style('display', 'inline')
+              .style('position', 'absolute')
+              .style('width', '250px')
+              .style('height', '50px')
+	      .style('visibility', 'visible')
+	      .style('opacity', '0.75')
+	      .style('background-color', '#590000')
+	      .style('color', '#ffffff')
+              .style('left', (event.pageX + 20) + 'px')
+              .style('top', (event.pageY + 20) + 'px')
+	      .attr('data-dependency', datum['dependency'])
+	      .html(`<p>${datum['dependency']}<\/p>`);
+          })
+        .on('mousemove', (event, datum) =>
+          {
+            tooltip
+              .style('left', (event.pageX + 20) + 'px')
+              .style('top', (event.pageY + 20) + 'px');
+          })
+        .on('mouseout mouseleave', (event, datum) =>
+          {
+            tooltip
+              .style('opacity', '0')
+	      .style('display', 'none')
+	      .style('visibility', 'hidden');
+          })
+        .call(drag(simulation));
+
+  function ticked() {
+    link
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y);
+
+    node
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y);
+  }
+
+  function drag(simulation) {
+    function dragstarted(event) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      event.subject.fx = event.subject.x;
+      event.subject.fy = event.subject.y;
+    }
+
+    function dragged(event) {
+      event.subject.fx = event.x;
+      event.subject.fy = event.y;
+    }
+
+    function dragended(event) {
+      if (!event.active) simulation.alphaTarget(0);
+      event.subject.fx = null;
+      event.subject.fy = null;
+    }
+
+    return d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+  }
 });
